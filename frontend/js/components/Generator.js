dedc74c39fd6fe884a74b226997446e2a1e45f37
Nirav Doshi <3119359+niravdd@users.noreply.github.com>
2026-02-26 18:25:39 +1100

    More Samples

--- frontend/js/components/Generator.js
+++ frontend/js/components/Generator.js
@@ -141,7 +141,7 @@
                                     </svg>
                                     Generate
                                 </button>
-                                <button id="btn-reset" class="btn btn-secondary btn-lg text-base">
+                                <button id="btn-reset" class="btn btn-lg text-base bg-amber-600 hover:bg-amber-500 text-white">
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>
                                     </svg>
